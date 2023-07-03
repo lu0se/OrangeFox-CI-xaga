36ca1c6df19997295c3bb2984f7b0107df1d3d4f
lu0se <717379060@qq.com>
2023-07-04 00:28:52 +0800

    first commit

---
+++ config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/D8100-9000-TWRP-Device-Tree/device_xiaomi_xaga-TWRP"
+
+export DEVICE="xaga"
+export OEM="xiaomi"
+
+# Build Target
+## "recoveryimage" - for A-Only Devices without using Vendor Boot
+## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
+## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
+export TARGET="vendorbootimage"
+
+export OUTPUT="OrangeFox*.zip"
+
+# Additional Dependencies (eg: Kernel Source)
+# Format: "repo dest"
+DEPS=(
+    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+)
+
+# Extra Command
+export EXTRA_CMD="export OF_MAINTAINER=lu0se"
+
+# Magisk
+## Use the Latest Release of Magisk for the OrangeFox addon
+export OF_USE_LATEST_MAGISK=true
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path.
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
